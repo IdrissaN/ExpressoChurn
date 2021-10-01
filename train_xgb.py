5bc5adddbd8b236cd1475007aff87f55bdb57492
Idrissa <idy447@gmail.com>
2021-10-01 23:53:35 +0200

    v2_1815 with xgb

--- train_xgb.py
+++ train_xgb.py
@@ -14,9 +14,9 @@ warnings.filterwarnings("ignore")
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_path', help='path of Train', type=str, default='data/Train_FE.csv', required=True)
-    parser.add_argument('--test_path', help='path of Test', type=str, default='data/Test_FE.csv', required=True)
-    parser.add_argument('--seed', default=128, type=int)
+    parser.add_argument('--train_path', help='path of Train', type=str, default='data/Train_meta.csv', required=True)
+    parser.add_argument('--test_path', help='path of Test', type=str, default='data/Test_meta.csv', required=True)
+    parser.add_argument('--seed', default=56, type=int)
     parser.add_argument('--shuffle', default=False, type=bool)
     parser.add_argument('--n_splits', default=5, type=int)
     args = parser.parse_args()
@@ -29,7 +29,6 @@ def train_model(train, test, features, target, n_splits, seed):
 
     oofs = np.zeros(train.shape[0])
     preds = np.zeros(test.shape[0])
-    models = []
 
     skf = StratifiedKFold(n_splits=n_splits, shuffle=args.shuffle, random_state=seed)
 
@@ -41,15 +40,16 @@ def train_model(train, test, features, target, n_splits, seed):
         trn_x, trn_y = trn[features], y.iloc[trn_idx]
         val_x, val_y = val[features], y.iloc[val_idx]
 
-        optuna_params = {'n_estimators': 2729, 
+        best_params = {'n_estimators': 2729, 
                         'max_depth': 10, 
                         #'learning_rate': 0.039437933897745005, 
                         'learning_rate': 0.01,
                         #'subsample': 0.8665852046914723, 
                         'subsample': 0.8,
                         #'colsample_bytree': 0.791939751295911, 
-                        'colsample_bytree': 0.7,
-                        'gamma': 0, 'max_bin': 214, 
+                        'colsample_bytree': 0.4,
+                        'gamma': 0, 
+                        #'max_bin': 214, 
                         #'reg_lambda': 1.2202681039175074, 
                         'reg_lambda': 1.22,
                         #'reg_alpha': 6.146787631363264,
@@ -58,9 +58,10 @@ def train_model(train, test, features, target, n_splits, seed):
                         'seed':1202,
                         'eval_metric': 'auc',
                         'gpu_id': 0,
-                        'tree_method': 'gpu_hist',}
+                        'tree_method': 'gpu_hist',
+                        }
     
-        clf = XGBClassifier(**optuna_params)
+        clf = XGBClassifier(**best_params)
     
         clf.fit(trn_x, trn_y, 
                 eval_set= [(trn_x, trn_y), (val_x, val_y)], 
@@ -70,8 +71,6 @@ def train_model(train, test, features, target, n_splits, seed):
         
         oofs[val_idx] = clf.predict_proba(val_x)[:, 1]
         preds += clf.predict_proba(test[features])[:, 1] / skf.n_splits
-
-        #models.append(clf)
     
         print(f'Fold {fold + 1} ROC AUC Score : {eval_auc(val_y, oofs[val_idx])}')
         del trn, val
@@ -89,14 +88,12 @@ if __name__=='__main__':
     args = get_args()
     seed_everything(args.seed)
     
-    train = pd.read_csv(args.train_path)
-    test = pd.read_csv(args.test_path)
+    train = pd.read_pickle(args.train_path)
+    test = pd.read_pickle(args.test_path)
 
     y = train.CHURN
 
-    excluded_feats = ['CHURN', 'user_id', 'REGION', 'TENURE', 'CD_TENURE', 'MRG', 'TOP_PACK']
-    #excluded_feats.extend(cfg.MEAN_FEATS)
-    #excluded_feats.extend(cfg.DIFF_MEAN_FEATS)
+    excluded_feats = ['CHURN', 'user_id', 'REGION', 'TENURE', 'CD_TENURE', 'MRG', 'TOP_PACK', 'ARPU_SEGMENT', 'REGULARITY_BIN', 'REGION_TENURE']
     excluded_feats.extend(cfg.DIFF_QRTLS_FEATS)
     excluded_feats.extend(cfg.LOG_FEATS)
 
@@ -107,5 +104,5 @@ if __name__=='__main__':
     submission = pd.DataFrame({'user_id': test.user_id, 'CHURN': preds})
     oof = pd.DataFrame({'user_id': train.user_id, 'CHURN': y, 'OOF': oofs})
 
-    submission.to_csv(os.path.join(cfg.submissions_path, f"sub_xgb_feats{len(features)}_cv{str(score).split('.')[1][:6]}_spl{args.n_splits}_seed{args.seed}_v2.csv"), index=False)
-    oof.to_csv(os.path.join(cfg.submissions_path, f"oof_xgb_feats{len(features)}_cv{str(score).split('.')[1][:6]}_spl{args.n_splits}_seed{args.seed}_v2.csv"), index=False)
+    submission.to_csv(os.path.join(cfg.submissions_path, f"sub_xgb_feats{len(features)}_cv{str(score).split('.')[1][:7]}_spl{args.n_splits}_seed{args.seed}_cs4_FE_v2.csv"), index=False)
+    oof.to_csv(os.path.join(cfg.submissions_path, f"oof_xgb_feats{len(features)}_cv{str(score).split('.')[1][:7]}_spl{args.n_splits}_seed{args.seed}_cs4_FE_v2.csv"), index=False)
